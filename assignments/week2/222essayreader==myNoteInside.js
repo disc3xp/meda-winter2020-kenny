f5b7db2a7a555f8860c05cf0241bec33aca23e92
Kenny <cqj15xp@gmail.com>
2020-11-03 23:37:48 -0800

    Worked more on essayreader.js and finished week2-homework2 Q&A

--- assignments/week2/222essayreader==myNoteInside.js
+++ assignments/week2/222essayreader==myNoteInside.js
@@ -4,12 +4,18 @@ const fs = require("fs");
 // process.argv has the command line information that was used to run this script.
 let arguments = process.argv;
 
+let history;
+
 // Check if we have an existing file called "history.txt"
 if (fs.existsSync("history.txt")) {
-    let history = fs.readFileSync("history.txt", "utf-8");
+    history = fs.readFileSync("history.txt", "utf-8");
 } else {
-    fs.writeFileSync("history.txt", "", "utf-8");
+    // ..if it doesn't, we creat the file, and write an empty to it.
+    fs.writeFileSync("history.txt", "", "utf-8");     //3 parts here, "filename.txt, "content", "utf-8"
 }
+
+
+// const serverPassword = 1234567;
 
 // console.log(arguments);
 
@@ -46,15 +52,25 @@ for (let i = 0; i < contentArray.length; i++) {
 
 }
 
-console.log(`The file ${fileName} contains a total of ${letterCount} letters.`);
+let firstSentence = `The file ${fileName} contains a total of ${letterCount} letters.`;
 
+console.log(firstSentence);
+
+// Same as history = history + firstSentence
+history += firstSentence;       // "\n"  ===> new line
 // Count Letters END
 
 // Count Words START
 
 let wordArray = fileContents.split(" ");
 
-console.log(`It has a total of ${wordArray.length} words in it.`);
+let secondSentence = `It has a total of ${wordArray.length} words in it.`;
+
+console.log(secondSentence);
+
+history = history + "\n" + secondSentence;   // "\n"  ===> we want to style, and make 2nd sentence into NEXT NEW LINE
+
+
 
 // Count Words END
 
@@ -70,4 +86,17 @@ for (let i = 0; i < contentArray.length; i++) {
     }
 }
 
-console.log(`It has a total of ${sentenceCount} sentences in it.`);
+let thirdSentence = `It has a total of ${sentenceCount} sentences in it.`;
+
+
+// let exampleURL = "http://www.google.com";
+
+console.log(thirdSentence);
+
+history = history + "\n" + thirdSentence + "\n\n";   // "\n" we want to style it. And making a empty line after each set setneces
+
+// Save the string in the history variable to the file.
+
+console.log("");
+console.log("Statistics saved to history!")
+fs.writeFileSync("history.txt", history, "utf-8");
